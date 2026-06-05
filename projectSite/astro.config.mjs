6cf79d3da020629cacc83a12a667a90c1d4c137f
0% [█ █ █ █ █ █ █ █ █ █] 100% <lild31415@gmail.com>
2026-06-05 16:58:22 -0500

    Manually force install cloudflare and marked dependencies

--- projectSite/astro.config.mjs
+++ projectSite/astro.config.mjs
@@ -1,23 +1,8 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
-import node from '@astrojs/node';
-
-// https://astro.build/config
+// https://astro.build
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: "standalone"
-  }),
-    // This server block configures BOTH the dev server AND the final production server
-  server: {
-    host: true, // This is the key! It means "listen on all network interfaces"
-    port: 4321, // You can explicitly set the port here
-  },
-  // Your vite config for playit.gg might still be needed
-  vite: {
-    preview: {
-      host: true,
-      allowedHosts: ['projhosting.playit.plus'],
-    },
-  },
+  adapter: cloudflare()
 });
