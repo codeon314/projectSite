5a07aeabbc4e53e1a78849d107bd17f98913edcf
0% [█ █ █ █ █ █ █ █ █ █] 100% <lild31415@gmail.com>
2026-06-07 19:08:37 -0500

    Added hidden status

---
+++ projectSite/functions/hidden_status.js
@@ -0,0 +1,15 @@
+export async function onRequestGet(context) {
+    // Set this to "1" or "0" depending on the desired output
+    const status = "1";
+
+    return new Response(status, {
+        status: 200,
+        headers: {
+            "Content-Type": "text/plain",
+            // Aggressive cache-busting headers to ensure Cloudflare always serves the latest value
+            "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+            "Pragma": "no-cache",
+            "Expires": "0"
+        }
+    });
+}
